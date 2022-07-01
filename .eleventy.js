84b67bac9bfb77adb04b39924b0199aae73eef46
Henry Catalini Smith <henry@catalinismith.com>
2022-07-01 21:50:20 +0200

    idk what im driving at with this but its fun

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
+const path = require("path")
+
 module.exports = function(eleventyConfig) {
+ eleventyConfig.addPassthroughCopy("*.js");
  eleventyConfig.addTemplateFormats("js");
  eleventyConfig.addExtension("js", {
   outputFileExtension: "html",
+  getData: async function(inputPath) {
+   return {
+    layout: "javascript",
+    title: path.basename(inputPath, ".js"),
+   }
+  },
   compile: async function(inputContent) {
    return async (data) => {
     return inputContent
---
+++ qsa.js
@@ -0,0 +1,1 @@
+export const qsa = qs => document.querySelectorAll(qs)
